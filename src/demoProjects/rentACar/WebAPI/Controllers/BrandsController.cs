75d5f1055f83cac7dc28f2383c4c621c1c532fd5
emojuq <gamertremre@gmail.com>
2022-09-09 18:37:12 +0300

    Senior C# 3. Gün 3. Kısım CQRS Devam

--- src/demoProjects/rentACar/WebAPI/Controllers/BrandsController.cs
+++ src/demoProjects/rentACar/WebAPI/Controllers/BrandsController.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Features.Brands.Commands.CreateBrand;
 using Application.Features.Brands.Dtos;
 using Application.Features.Brands.Models;
+using Application.Features.Brands.Queries.GetByIdBrand;
 using Application.Features.Brands.Queries.GetListBrand;
 using Core.Application.Requests;
 using Microsoft.AspNetCore.Http;
@@ -26,12 +27,22 @@ namespace WebAPI.Controllers
 
 
         [HttpGet]
-        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest) // GET OPERASYONLARINDA QUERYDEN OKUDUGUMUZ İÇİN FROMQUERY
         {
+            //İlk önce pagerequest olarak aldık sonrasında gelen sonucu getlistbrandqueye döndürdük..
             GetListBrandQuery getListBrandQuery = new() { pageRequest = pageRequest };
             BrandListModel result=await Mediator.Send(getListBrandQuery);
             return Ok(result);
            
         }
+
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetById([FromRoute] GetByIdBrandQuery getByIdBrandQuery) // direk url üzerinden okuma yapılacağı için fromroute
+        {
+            BrandGetByIdDto brandGetByIdDto = await Mediator.Send(getByIdBrandQuery);
+            return Ok(brandGetByIdDto);
+        } 
+
     }
 }
