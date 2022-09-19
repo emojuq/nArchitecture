e19b850ea6a6606b8b5ddf3e29df0cad3ef4e2c8
emojuq <gamertremre@gmail.com>
2022-09-19 15:54:48 +0300

    Senior C# 4. Gün 1. Kısım Models Entity'si projeye eklenmesi

--- src/demoProjects/rentACar/Domain/Entities/Brand.cs
+++ src/demoProjects/rentACar/Domain/Entities/Brand.cs
@@ -12,6 +12,8 @@ namespace Domain.Entities
        
         public string Name { get; set; }
 
+        public virtual ICollection<Model> Models { get; set; }
+
         public Brand()
         {
         }
--- src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -13,6 +13,7 @@ namespace Persistence.Contexts
     {
         protected IConfiguration Configuration { get; set; }
         public DbSet<Brand> Brands { get; set; }
+        public DbSet<Model> Models { get; set; }
 
 
         public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
@@ -34,6 +35,23 @@ namespace Persistence.Contexts
                 a.ToTable("Brands").HasKey(k => k.Id);
                 a.Property(p => p.Id).HasColumnName("Id");
                 a.Property(p => p.Name).HasColumnName("Name");
+
+                a.HasMany(p=>p.Models); //brand'in bir sürü modeli var anlamına gelir Örnek; Bmw=>3.20,7.40
+
+            });
+
+
+            modelBuilder.Entity<Model>(a =>
+            {
+                a.ToTable("Models").HasKey(k => k.Id);
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.BrandId).HasColumnName("BrandId");
+                a.Property(p => p.Name).HasColumnName("Name");
+                a.Property(p => p.DailyPrice).HasColumnName("DailyPrice");
+                a.Property(p => p.ImageUrl).HasColumnName("ImageUrl");
+
+                a.HasOne(p => p.Brand); //modelin bir tane brand'i var anlamına gelir Örnek;3.20 => Bmw
+
             });
 
 
@@ -42,6 +60,8 @@ namespace Persistence.Contexts
             modelBuilder.Entity<Brand>().HasData(brandEntitySeeds);
 
 
+            Model[] modelEntitySeeds = { new(1, 1, "Series 4", 1500, "") ,  new(2, 1, "Series 3", 1200, ""),  new(3, 2, "A180", 1000, "") };
+            modelBuilder.Entity<Model>().HasData(modelEntitySeeds);
         }
     }
 }
