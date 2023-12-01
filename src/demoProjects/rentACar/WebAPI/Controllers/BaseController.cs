e0d9b1720c4df061ffc5e9b6a527378d414cfe19
emojuq <gamertremre@gmail.com>
2023-12-02 01:02:21 +0300

    5. Gün Ders 3

--- src/demoProjects/rentACar/WebAPI/Controllers/BaseController.cs
+++ src/demoProjects/rentACar/WebAPI/Controllers/BaseController.cs
@@ -8,6 +8,13 @@ namespace WebAPI.Controllers
         protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
         private IMediator? _mediator;
 
+        protected string? GetIpAddress()
+        {
+            if (Request.Headers.ContainsKey("X-Forwarded-For")) return Request.Headers["X-Forwarded-For"];
+            return HttpContext.Connection.RemoteIpAddress?.MapToIPv4().ToString();
+        }
+
+
         //Bir base controller oluştuuryoruz.Çünkü aslında tüm controllerlarda mediator kullanıcağız.Bu da sürekli oluşturulacak ve kontorl edilemeyeceği için bir şartlandırma koyuyoruz
         // Ve bu şekilde mediator nesnesi varsa yenisi oluşturulmuyor.
     }
